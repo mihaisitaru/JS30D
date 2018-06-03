f898c564d623573cd12c8f5ec532e70924a5ed35
mihaisitaru <mihai.sitaru@yahoo.com>
2018-06-03 23:12:05 +0300

    JS Drums

---
+++ JS Drums/script.js
@@ -0,0 +1,22 @@
+(function () {
+    document.addEventListener('keydown', drums);
+
+    function drums(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        // skip function if no audio on keyCode
+        if(!audio) return;
+        // rewind audio to the start
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+    }
+
+    function removeTransition(e) {
+        if (e.propertyname === 'transform') return;
+        this.classList.remove('playing');
+    }
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+})();
